ffb971ce5cad59d43f32004431be966cdc8e5f19
ntim <timotic.nikola.27@gmail.com>
2022-05-17 22:07:48 +0200

    add test & deleteUser

--- DOM/search_&_deleteUser/index.js
+++ DOM/search_&_deleteUser/index.js
@@ -1,8 +1,12 @@
 const divIspisKorisnika = document.createElement('div')
 divIspisKorisnika.classList.add('container')
 
+const inputSearch = document.createElement('input')
+inputSearch.type = 'text'
+inputSearch.placeholder = 'Search...'
+
 const body = document.body
-body.appendChild(divIspisKorisnika)
+body.append(inputSearch, divIspisKorisnika)
 
 let korisnici = [
     {
@@ -38,22 +42,22 @@ let korisnici = [
 const renderBase = () => {
     const divHead = document.createElement('div')
     divHead.classList.add('korisnik')
-    
+
     const pIme = document.createElement('p')
     pIme.textContent = 'IME'
-    
+
     const pPrezime = document.createElement('p')
     pPrezime.textContent = 'PREZIME'
-    
+
     const pGodine = document.createElement('p')
     pGodine.textContent = 'GODINE'
-    
+
     const pZanimanje = document.createElement('p')
     pZanimanje.textContent = 'ZANIMANJE'
-    
+
     const pPlata = document.createElement('p')
     pPlata.textContent = 'PLATA'
-    
+
     divHead.append(
         pIme,
         pPrezime,
@@ -61,43 +65,40 @@ const renderBase = () => {
         pZanimanje,
         pPlata
     )
-    
+
     divIspisKorisnika.appendChild(divHead)
 }
 
-const renderUser = () => {
+const renderUser = (korisnici) => {
     korisnici.forEach(korisnik => {
         const divKorisnik = document.createElement('div')
         divKorisnik.classList.add('korisnik')
-    
+
         const pIme = document.createElement('p')
         pIme.textContent = korisnik.ime
-    
+
         const pPrezime = document.createElement('p')
         pPrezime.textContent = korisnik.prezime
-    
+
         const pGodine = document.createElement('p')
         pGodine.textContent = korisnik.godine
-    
+
         const pZanimanje = document.createElement('p')
         pZanimanje.textContent = korisnik.zanimanje
-    
+
         const pPlata = document.createElement('p')
         pPlata.textContent = korisnik.plata
-    
+
         const buttonBrisiKorisnika = document.createElement('button')
         buttonBrisiKorisnika.textContent = 'Obrisi'
-    
+
         buttonBrisiKorisnika.addEventListener('click', (e) => {
-            // console.log(e);
-            // divKorisnik.classList.add('hide')
             korisnici.splice(korisnici.indexOf(korisnik), 1)
             divIspisKorisnika.textContent = ''
-            // console.log(korisnici)
             renderBase()
-            renderUser()
+            renderUser(korisnici)
         })
-    
+
         divKorisnik.append(
             pIme,
             pPrezime,
@@ -106,11 +107,17 @@ const renderUser = () => {
             pPlata,
             buttonBrisiKorisnika
         )
-    
+
         divIspisKorisnika.appendChild(divKorisnik)
     })
 }
 
 renderBase()
-renderUser()
+renderUser(korisnici)
+
+inputSearch.addEventListener('keyup', event => {
+    divIspisKorisnika.textContent = ''
+    renderBase()
+    renderUser(korisnici.filter(korisnik => korisnik.ime.includes(inputSearch.value)))
+})
 
